81e18ab371405000b6439c2f3bdeb164d847b3e0
SylvainTran <45033804+SylvainTran@users.noreply.github.com>
2023-02-16 13:32:10 -0500

    Add angular mat lib and flexbox layout test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
