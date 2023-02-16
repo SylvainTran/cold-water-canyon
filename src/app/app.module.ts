438f9055645c64b6034933dbda957f7e1a7a8cd4
SylvainTran <45033804+SylvainTran@users.noreply.github.com>
2023-02-16 17:47:00 -0500

    Add new icons and changed primary font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { PartDispatcherService } from './core/part-dispatcher.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatGridListModule
+    MatGridListModule,
+    MatIconModule
   ],
   providers: [PageControllerService, PartDispatcherService],
   bootstrap: [AppComponent]
